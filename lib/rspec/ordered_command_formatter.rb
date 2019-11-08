5bb8711f1b58e4ba32e3059c7200a15bb41e3bc9
David Stosik <david.stosik+git-noreply@gmail.com>
2019-11-09 00:15:13 +0900

    Implement formatter

--- lib/rspec/ordered_command_formatter.rb
+++ lib/rspec/ordered_command_formatter.rb
@@ -1,8 +1,32 @@
 require "rspec/ordered_command_formatter/version"
+RSpec::Support.require_rspec_core "formatters/base_formatter"
 
-module Rspec
-  module OrderedCommandFormatter
-    class Error < StandardError; end
-    # Your code goes here...
+module RSpec
+  class OrderedCommandFormatter
+    RSpec::Core::Formatters.register self, :seed, :dump_summary
+
+    def seed(notification)
+      self.seed_value = notification.seed if notification.seed_used?
+    end
+
+    def dump_summary(notification)
+      output.puts [
+        "rspec",
+        order_option,
+        notification.examples.map(&:location)
+      ].flatten.join(" ")
+    end
+
+    private
+
+      attr_accessor :seed_value
+
+      def order_option
+        if seed_value
+          "--order random:#{seed_value}"
+        else
+          "--order defined"
+        end
+      end
   end
 end
