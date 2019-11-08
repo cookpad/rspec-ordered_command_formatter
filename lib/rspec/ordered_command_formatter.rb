8c1af09998fc4d840adb7e8c9b25bea8c97b0310
David Stosik <david.stosik+git-noreply@gmail.com>
2019-11-09 00:14:39 +0900

    bundle gem rspec-ordered_command_formatter

---
+++ lib/rspec/ordered_command_formatter.rb
@@ -0,0 +1,8 @@
+require "rspec/ordered_command_formatter/version"
+
+module Rspec
+  module OrderedCommandFormatter
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/rspec/ordered_command_formatter/version.rb
@@ -0,0 +1,5 @@
+module Rspec
+  module OrderedCommandFormatter
+    VERSION = "0.1.0"
+  end
+end
